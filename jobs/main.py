f6c2ee5df41957ec73130cbfb8cfe19b1b16cb1e
mauhcs <maukeshigomu@gmail.com>
2020-07-19 20:29:32 +0900

    Sun Jul 19 20:29:32 JST 2020

--- jobs/main.py
+++ jobs/main.py
@@ -4,5 +4,7 @@ import datetime
 
 if __name__ == "__main__":
   while True:
-    print("Awesome jobs running @", datetime.datetime.now())
+    with open("/tmp/superpython.log", "a") as f:
+      print("Awesome jobs running @", datetime.datetime.now())
+      print("Awesome jobs running @", datetime.datetime.now(), file=f)
     time.sleep(5)
