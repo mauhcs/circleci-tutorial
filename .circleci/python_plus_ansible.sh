ac63b6dce9690b57b490699bcdb23c4383500419
mauhcs <maukeshigomu@gmail.com>
2020-07-19 14:48:04 +0900

    Sun Jul 19 14:48:04 JST 2020

--- .circleci/python_plus_ansible.sh
+++ .circleci/python_plus_ansible.sh
@@ -1,5 +1,5 @@
 
-sudo apt update
+
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
 bash ~/miniconda.sh -b -p $HOME/miniconda
 echo export PATH="$HOME/miniconda/bin:$PATH" >> ~/.bashrc
